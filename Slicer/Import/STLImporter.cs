633f830bf6c912f2de719eb246f754c7445913a8
FlashyFire <rizotto@list.ru>
2019-02-17 00:04:07 +0300

    Import

---
+++ Slicer/Import/STLImporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Slicer.ObjectModel;
+using System.IO;
+
+namespace Slicer.Import
+{
+    class STLImporter
+    {
+        private readonly Model model;
+
+        public STLImporter(Model model) {
+            if (model == null)
+                throw new ArgumentNullException("model should be not null!");
+            this.model = model;
+        }
+
+        public void Import(String fileName)
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                Import(fileStream);
+            }
+        } 
+
+        public void Import(Stream stream)
+        {
+
+        }
+    }
+}
