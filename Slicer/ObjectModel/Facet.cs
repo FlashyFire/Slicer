0a688ef23408833aa8d1acda2d04a9434a3ee2d3
FlashyFire <rizotto@list.ru>
2019-02-16 23:40:35 +0300

    Model

---
+++ Slicer/ObjectModel/Facet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Slicer.ObjectModel
+{
+    class Facet
+    {
+        public Vector3 Normal;
+        public Vector3 Vertex1;
+        public Vector3 Vertex2;
+        public Vector3 Vertex3;
+    }
+}
---
+++ Slicer/ObjectModel/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slicer.ObjectModel
+{
+    class Model
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<Facet> Facets { get; } = new List<Facet>();
+
+        public void Clear() {
+            Name = String.Empty;
+            Facets.Clear();
+        }
+    }
+}
